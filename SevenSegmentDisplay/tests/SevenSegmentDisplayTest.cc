5b228407796197628942d9216764d79a1cd2740f
Ilya Shmygol <ishmygol@gmail.com>
2017-01-29 18:40:14 +0100

    Comm#2: Dummy tests for SevenSegmentDisplay

---
+++ SevenSegmentDisplay/tests/SevenSegmentDisplayTest.cc
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "../SevenSegmentDisplay.h"
+ 
+TEST(SevenSegmentDisplayTest, Sample) { 
+    EXPECT_EQ(18.0, 18.0);
+    EXPECT_EQ(18.0, 18.0);
+    EXPECT_EQ(18.0, 18.0);
+    EXPECT_EQ(18.0, 18.0);
+}
+
+TEST(SevenSegmentDisplayTest, Sample2) { 
+    EXPECT_EQ(18.0, 18.0);
+    EXPECT_EQ(18.0, 18.0);
+    EXPECT_EQ(18.0, 18.0);
+    EXPECT_EQ(18.0, 18.0);
+}
+
